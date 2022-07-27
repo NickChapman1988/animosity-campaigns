500b5760205073ff0835f29f54947c6f2a887387
NickChapman1988 <muppetnj_uk@hotmail.com>
2022-07-27 15:46:38 +0000

    Setup django-allauth

--- animosity_campaigns/settings.py
+++ animosity_campaigns/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-nickchapman-animosityca-b6ny4unhhgr.ws-eu54.gitpod.io/']
+CSRF_TRUSTED_ORIGINS = ['https://8000-nickchapman-animosityca-b6ny4unhhgr.ws-eu54.gitpod.io']
 
 # Application definition
 
@@ -81,6 +81,16 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'animosity_campaigns.wsgi.application'
 
