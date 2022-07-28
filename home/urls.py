117243f7944b8185e5e447d80ef2c3fc5432dcbe
NickChapman1988 <muppetnj_uk@hotmail.com>
2022-07-28 20:29:06 +0000

    Added home app and templates

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+"""animosity_campaigns URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
